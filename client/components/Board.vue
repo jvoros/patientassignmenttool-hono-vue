9abd32f6a16746b3ee3eca3213c2a9c6dbc28907
Jeremy Voros <jeremyvoros@gmail.com>
2025-07-07 23:03:48 -0600

    fixing grid

--- client/components/Board.vue
+++ client/components/Board.vue
@@ -5,59 +5,58 @@ import Zone from "./Zone.vue";
 </script>
 
 <template>
-  <main class="grid container">
-    <section class="grid-timeline">
-      <Timeline />
-    </section>
-    <section class="grid-main">
-      <Zone :slug="board.zoneOrder[0]" />
-    </section>
-    <section class="grid-other">
-      <Zone v-for="slug in board.zoneOrder.slice(1)" :slug="slug" />
-    </section>
-  </main>
+    <main class="grid container">
+        <section class="grid-timeline">
+            <Timeline />
+        </section>
+        <section class="grid-main">
+            <Zone :slug="board.zoneOrder[0]" />
+        </section>
+        <section class="grid-other">
+            <Zone v-for="slug in board.zoneOrder.slice(1)" :slug="slug" />
+        </section>
+    </main>
 </template>
 
 <style scoped>
 main {
-  display: grid;
-  grid-template-areas:
-    "main"
-    "other"
-    "time";
-  grid-gap: 0.5rem;
-  grid-template-columns: 4fr 5fr 4fr;
+    display: grid;
+    grid-template-areas:
+        "main"
+        "other"
+        "time";
 }
 
 @media screen and (min-width: 700px) {
-  main {
-    grid-template-areas:
-      "time main"
-      "time other";
-  }
+    main {
+        grid-template-areas:
+            "time main"
+            "time other";
+        grid-gap: 0.5rem;
+    }
 }
 
 @media screen and (min-width: 1200px) {
-  main {
-    grid-template-areas: "time main other";
-    grid-gap: 0;
-  }
+    main {
+        grid-template-areas: "time main other";
+        grid-template-columns: 4fr 5fr 4fr;
+    }
 }
 
 .grid-timeline {
-  grid-area: time;
+    grid-area: time;
 }
 
 .grid-main {
-  grid-area: main;
+    grid-area: main;
 }
 
 .grid-other {
-  grid-area: other;
+    grid-area: other;
 }
 
 section {
-  border: 0px solid lightgray;
-  padding: var(--padding);
+    border: 0px solid lightgray;
+    padding: var(--padding);
 }
 </style>
